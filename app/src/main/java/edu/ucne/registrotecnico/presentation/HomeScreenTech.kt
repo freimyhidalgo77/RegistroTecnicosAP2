0e8e9a5261372dd3c2780dbd0da859500424d2d1
fahh7 <fahh7@FREIMYHP>
2025-05-16 12:23:34 -0400

    Creando HomeScreen para el menu principal.

--- app/src/main/java/edu/ucne/registrotecnico/presentation/HomeScreenTech.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/HomeScreenTech.kt
@@ -1,2 +1,68 @@
-package edu.ucne.registrotecnico.presentation
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.*
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavHostController) {
+    val drawerState = rememberDrawerState(initialValue = DrawerValue.Open)
+    val scope = rememberCoroutineScope()
+
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    title = { Text("Agenda de Tecnicos") }
+                )
+            }
+        ) { padding ->
+
+                Column(
+                    modifier = Modifier
+                        .padding(padding)
+                        .padding(16.dp)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(16.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                )
+                {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .clickable { navController.navigate("tecnicos") }
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Text("Técnicos", fontSize = 18.sp)
+                        }
+                    }
+
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(100.dp)
+                            .clickable { navController.navigate("tickets") }
+                    ) {
+                        Box(
+                            contentAlignment = Alignment.Center,
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            Text("Tickets", fontSize = 18.sp)
+                        }
+                    }
+                }
+            }
+        }
+
 
