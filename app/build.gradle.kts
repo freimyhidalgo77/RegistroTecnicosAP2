169720daba5f46fdd7adc422675e0c837ed57f8b
fahh7 <fahh7@FREIMYHP>
2025-05-17 17:02:30 -0400

    Gradle y versiones

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation("androidx.room:room-runtime:2.6.1")
     implementation(libs.androidx.junit.ktx)
     implementation(libs.androidx.activity.ktx)
+    implementation(libs.androidx.benchmark.common)
     annotationProcessor("androidx.room:room-compiler:2.6.1")
     ksp("androidx.room:room-compiler:2.6.1")
 
