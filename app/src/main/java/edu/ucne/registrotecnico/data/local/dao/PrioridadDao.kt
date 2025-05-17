6f1e9f7c7018d12bbd68397f6927e3931fc5e09f
fahh7 <fahh7@FREIMYHP>
2025-05-17 17:04:44 -0400

    Prioridad Dao

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/dao/PrioridadDao.kt
@@ -0,0 +1,24 @@
+package edu.ucne.registrotecnico.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
+import edu.ucne.registrotecnico.data.local.entities.TicketEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PrioridadDao {
+    @Upsert
+    suspend fun save(prioridad: PrioridadEntity)
+
+    @Query("SELECT * FROM Prioridades WHERE prioridadId = :id LIMIT 1")
+    suspend fun find(id: Int): PrioridadEntity
+
+    @Delete
+    suspend fun delete(prioridad: PrioridadEntity)
+
+    @Query("SELECT * FROM Prioridades")
+    fun getAll(): Flow<List<PrioridadEntity>>
+}
