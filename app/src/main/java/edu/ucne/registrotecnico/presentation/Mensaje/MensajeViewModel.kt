f290d06f4cf58958f2df789844929c055118ee98
fahh7 <fahh7@FREIMYHP>
2025-05-23 14:47:07 -0400

    Corrigiendo el viewModel

--- app/src/main/java/edu/ucne/registrotecnico/presentation/Mensaje/MensajeViewModel.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/Mensaje/MensajeViewModel.kt
@@ -9,61 +9,33 @@ import edu.ucne.registrotecnico.data.local.entities.TicketEntity
 import edu.ucne.registrotecnico.data.repository.MensajeRepository
 import edu.ucne.registrotecnico.data.repository.TecnicosRepository
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import java.util.Date
 import javax.inject.Inject
 
+
+
+data class MensajeUiState(
+    val descripcion: String = "",
+    val fecha: Date? = null
+)
+
 @HiltViewModel
-class MensajeViewModel @Inject constructor(
-    private val mensajeRepository: MensajeRepository,
-    private val tecnicoRepository: TecnicosRepository
+class MensajeViewModel @Inject constructor() : ViewModel() {
+    private val _uiState = MutableStateFlow(MensajeUiState())
+    val uiState: StateFlow<MensajeUiState> = _uiState
 
-):ViewModel(){
+    private val _mensajes = MutableStateFlow<List<MensajeEntity>>(emptyList())
+    val mensajes: StateFlow<List<MensajeEntity>> = _mensajes
 
-    private val _uiState = MutableStateFlow(MensajeEntity(0, 0, "","", Date()))
-    val uiState = _uiState.asStateFlow()
-
-
-    fun save(mensaje:MensajeEntity) {
-        viewModelScope.launch {
-            mensajeRepository.save(mensaje)
-        }
+    fun onDescripcionChange(nuevo: String) {
+        _uiState.value = _uiState.value.copy(descripcion = nuevo, fecha = Date())
     }
 
-
-    fun delete(mensaje: MensajeEntity) {
-        viewModelScope.launch {
-            mensajeRepository.delete(_uiState.value)
-        }
+    fun save(mensaje: MensajeEntity) {
+        _mensajes.value = _mensajes.value + mensaje
+        _uiState.value = MensajeUiState()
     }
-
-    fun find(id: Int) {
-        viewModelScope.launch {
-            val mensaje = mensajeRepository.find(id)
-            if (mensaje != null) {
-                _uiState.value = mensaje
-            }
-        }
-
-
-
-        data class UiState(
-            val ticket: TicketEntity = TicketEntity(0, "", 0, "", "", "", 0),
-            val mensajes: List<MensajeEntity> = emptyList(),
-            val nuevoMensaje: String = "",
-            val mensajeError: String? = null,
-            val mensajeExito: String? = null,
-            val remitente: String = ""
-        )
-
-    }
-
-    fun onDescripcionChange(newDescripcion: String) {
-        _uiState.value = _uiState.value.copy(descripcion = newDescripcion)
-    }
-
-
-
-
 }
