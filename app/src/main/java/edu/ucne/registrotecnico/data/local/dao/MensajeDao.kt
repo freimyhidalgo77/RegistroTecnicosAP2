43a10922bf85ec9c110ae0ae226755a4e386762e
fahh7 <fahh7@FREIMYHP>
2025-05-23 00:33:53 -0400

    Mensaje dao

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/dao/MensajeDao.kt
@@ -0,0 +1,25 @@
+package edu.ucne.registrotecnico.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Query
+import androidx.room.Upsert
+import edu.ucne.registrotecnico.data.local.entities.MensajeEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface MensajeDao {
+
+    @Upsert
+    suspend fun save(mensaje: MensajeEntity)
+
+    @Query("SELECT * FROM Mensajes WHERE  mensajeId = :id LIMIT 1")
+    suspend fun find(id: Int): MensajeEntity
+
+    @Delete
+    suspend fun delete(mensaje: MensajeEntity)
+
+    @Query("SELECT * FROM Mensajes")
+    fun getAll(): Flow<List<MensajeEntity>>
+
+}
