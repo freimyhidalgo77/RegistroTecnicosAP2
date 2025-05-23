430b8e818ce907cfafc748a960cc21fb5c6511c8
fahh7 <fahh7@FREIMYHP>
2025-05-23 00:32:59 -0400

    Update de la base de datos

--- app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/database/TecnicoDb.kt
@@ -2,9 +2,12 @@ package edu.ucne.registrotecnico.data.local.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import edu.ucne.registrotecnico.data.local.dao.MensajeDao
 import edu.ucne.registrotecnico.data.local.dao.PrioridadDao
 import edu.ucne.registrotecnico.data.local.dao.TecnicoDao
 import edu.ucne.registrotecnico.data.local.dao.TicketDao
+import edu.ucne.registrotecnico.data.local.entities.MensajeEntity
 import edu.ucne.registrotecnico.data.local.entities.PrioridadEntity
 import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
 import edu.ucne.registrotecnico.data.local.entities.TicketEntity
@@ -14,14 +17,18 @@ import edu.ucne.registrotecnico.data.local.entities.TicketEntity
         TecnicoEntity::class,
         TicketEntity::class,
         PrioridadEntity::class,
+        MensajeEntity::class,
+
     ],
-    version = 3,
+    version = 4,
     exportSchema = false
 )
 
+@TypeConverters(Converters::class)
 abstract class TecnicoDb : RoomDatabase() {
     abstract fun tecnicoDao(): TecnicoDao
     abstract fun ticketDao(): TicketDao
     abstract fun prioridadDao(): PrioridadDao
+    abstract fun mensajeDao(): MensajeDao
 
 }
