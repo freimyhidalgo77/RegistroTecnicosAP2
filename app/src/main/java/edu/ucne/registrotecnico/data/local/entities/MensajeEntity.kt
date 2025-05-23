058d2f7e1c52afaf126a542c87975bdcfb09b69c
fahh7 <fahh7@FREIMYHP>
2025-05-23 00:33:39 -0400

    Mensaje entity

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/local/entities/MensajeEntity.kt
@@ -0,0 +1,17 @@
+package edu.ucne.registrotecnico.data.local.entities
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.Date
+
+
+@Entity(tableName="Mensajes")
+data class MensajeEntity(
+    @PrimaryKey(autoGenerate = true)
+    val mensajeId: Int? = null,
+    val tecnicoId: Int? = null,
+    val remitente:String = "",
+    val descripcion: String = "",
+    val fecha: Date?
+
+)
