b2ac4a9184be7cd38e200d339d7ff6a542d5674e
fahh7 <fahh7@FREIMYHP>
2025-05-23 12:09:33 -0400

    Ajustando el ticket repository

--- app/src/main/java/edu/ucne/registrotecnico/data/repository/TicketRepository.kt
+++ app/src/main/java/edu/ucne/registrotecnico/data/repository/TicketRepository.kt
@@ -3,9 +3,10 @@ package edu.ucne.registrotecnico.data.repository
 import edu.ucne.registrotecnico.data.local.dao.TicketDao
 import edu.ucne.registrotecnico.data.local.entities.TicketEntity
 import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
 
 
-class TicketRepository(
+class TicketRepository  @Inject constructor(
 
     private val dao: TicketDao
 ) {
