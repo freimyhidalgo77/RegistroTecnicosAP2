06273dd611875dd807ec10a5116bc0865ad1d4b4
fahh7 <fahh7@FREIMYHP>
2025-05-23 11:54:17 -0400

    Creando un AppModule para arrancar Hilt con las entidades ya creadas.

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/di/AppModule.kt
@@ -0,0 +1,46 @@
+package edu.ucne.registrotecnico.data.di
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import edu.ucne.registrotecnico.data.local.dao.MensajeDao
+import edu.ucne.registrotecnico.data.local.database.TecnicoDb
+import dagger.hilt.android.qualifiers.ApplicationContext
+
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext applicationContext: Context): TecnicoDb {
+        return Room.databaseBuilder(
+            applicationContext,
+            TecnicoDb::class.java,
+            "TecnicoDb"
+        ).fallbackToDestructiveMigration().build()
+    }
+
+
+
+    @Provides
+    @Singleton
+    fun provideTecnicoDao(appDataDb: TecnicoDb) = appDataDb.tecnicoDao()
+
+    @Provides
+    @Singleton
+    fun provideTicketDao(appDataDb: TecnicoDb) = appDataDb.ticketDao()
+
+
+    @Provides
+    fun provideMensajeDao(db: TecnicoDb): MensajeDao {
+        return db.mensajeDao()
+    }
+
+
+}
