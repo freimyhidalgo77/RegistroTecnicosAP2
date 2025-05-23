cafccbe3a8d12da344cd77d0ea991358a5ff78e7
fahh7 <fahh7@FREIMYHP>
2025-05-23 00:32:42 -0400

    View Model de ticket con ui state

--- app/src/main/java/edu/ucne/registrotecnico/presentation/ticket/TicketViewModel.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/ticket/TicketViewModel.kt
@@ -2,19 +2,47 @@ package edu.ucne.registrotecnico.presentation.ticket
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import edu.ucne.registrotecnico.data.local.entities.MensajeEntity
 import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
 import edu.ucne.registrotecnico.data.local.entities.TicketEntity
 import edu.ucne.registrotecnico.data.repository.TecnicosRepository
 import edu.ucne.registrotecnico.data.repository.TicketRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class TicketViewModel(
-    private val ticketRepository: TicketViewModel
+@HiltViewModel
+class TicketViewModel @Inject constructor(
+    private val ticketRepository: TicketRepository,
+
 ): ViewModel() {
+
+    private val _uiState = MutableStateFlow(TicketEntity(0,"",0,"","","",0))
+    val uiState = _uiState.asStateFlow()
+
     fun saveTicket(ticket: TicketEntity) {
         viewModelScope.launch {
-            ticketRepository.saveTicket(ticket)
+            ticketRepository.save(ticket)
         }
     }
+
+    fun deleteTicket(ticket: TicketEntity) {
+        viewModelScope.launch {
+            ticketRepository.delete(_uiState.value)
+        }
+    }
+
+    fun find(id: Int) {
+        viewModelScope.launch {
+            val ticket = ticketRepository.find(id)
+            if (ticket != null) {
+                _uiState.value = ticket
+            }
+        }
+    }
+
+
 
 }
