90156865d09fc7d87743a6cd32c1bd7fa110ea6c
fahh7 <fahh7@FREIMYHP>
2025-05-24 01:16:50 -0400

    Corrigiendo metodos de save y eliminar mensajes para hacer dichas acciones

--- app/src/main/java/edu/ucne/registrotecnico/presentation/Mensaje/MensajeViewModel.kt
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/Mensaje/MensajeViewModel.kt
@@ -4,38 +4,77 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.ucne.registrotecnico.data.local.entities.MensajeEntity
-import edu.ucne.registrotecnico.data.local.entities.TecnicoEntity
-import edu.ucne.registrotecnico.data.local.entities.TicketEntity
 import edu.ucne.registrotecnico.data.repository.MensajeRepository
-import edu.ucne.registrotecnico.data.repository.TecnicosRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-import java.util.Date
+import java.util.*
 import javax.inject.Inject
 
-
-
 data class MensajeUiState(
+    val remitente: String = "",
     val descripcion: String = "",
-    val fecha: Date? = null
+    val fecha: Date? = null,
+    val rol: String = "Operador"
 )
 
 @HiltViewModel
-class MensajeViewModel @Inject constructor() : ViewModel() {
+class MensajeViewModel @Inject constructor(
+    private val mensajeRepository: MensajeRepository
+) : ViewModel() {
+
     private val _uiState = MutableStateFlow(MensajeUiState())
-    val uiState: StateFlow<MensajeUiState> = _uiState
+    val uiState: StateFlow<MensajeUiState> = _uiState.asStateFlow()
 
     private val _mensajes = MutableStateFlow<List<MensajeEntity>>(emptyList())
-    val mensajes: StateFlow<List<MensajeEntity>> = _mensajes
+    val mensajes: StateFlow<List<MensajeEntity>> = _mensajes.asStateFlow()
+
+    init {
+        getMensajes()
+    }
+
+    private fun getMensajes() {
+        viewModelScope.launch {
+            mensajeRepository.getAll().collect {
+                _mensajes.value = it
+            }
+        }
+    }
 
     fun onDescripcionChange(nuevo: String) {
         _uiState.value = _uiState.value.copy(descripcion = nuevo, fecha = Date())
     }
 
-    fun save(mensaje: MensajeEntity) {
-        _mensajes.value = _mensajes.value + mensaje
-        _uiState.value = MensajeUiState()
+    fun onRemitenteChange(nuevo: String) {
+        _uiState.value = _uiState.value.copy(remitente = nuevo)
     }
+
+    fun save() {
+        val mensaje = MensajeEntity(
+            remitente = _uiState.value.remitente,
+            descripcion = _uiState.value.descripcion,
+            fecha = _uiState.value.fecha ?: Date(),
+            rol = _uiState.value.rol
+        )
+        viewModelScope.launch {
+            mensajeRepository.save(mensaje)
+            _uiState.value = MensajeUiState()
+        }
+    }
+
+    fun deleteMensaje(mensaje: MensajeEntity) {
+        viewModelScope.launch {
+            mensajeRepository.delete(mensaje)
+            getMensajes()
+        }
+    }
+
+    fun onRolChange(newRol: String) {
+        _uiState.update { it.copy(rol = newRol) }
+    }
+
+
+
 }
