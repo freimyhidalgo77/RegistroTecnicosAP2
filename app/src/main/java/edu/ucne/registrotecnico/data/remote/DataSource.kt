e3134f7a39ac7ceeac94ae0d187ff0996552ee56
fahh7 <fahh7@FREIMYHP>
2025-06-12 13:26:18 -0400

    Cliente DataSource

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/remote/DataSource.kt
@@ -0,0 +1,21 @@
+package edu.ucne.registrotecnico.data.remote
+
+import edu.ucne.registrotecnico.data.remote.dto.ClienteDTO
+import edu.ucne.registrotecnico.data.remote.dto.RetencionDTO
+import edu.ucne.registrotecnico.data.remote.retenciones.RetencionManajerApi
+import javax.inject.Inject
+
+class DataSource @Inject constructor(
+    private val clientesManagerApi: ClienteManajerApi
+){
+    suspend fun getClientes() = clientesManagerApi.getClientes()
+
+    suspend fun getCliente(id: Int) = clientesManagerApi.getClientes(id)
+
+    suspend fun saveCliente(clienteDTO: ClienteDTO) = clientesManagerApi.saveClientes(clienteDTO)
+
+    suspend fun actualizarClientes(id: Int, clienteDto: ClienteDTO) = clientesManagerApi.actualizarCliente(id, clienteDto)
+
+    suspend fun deleteCliente(id: Int) = clientesManagerApi.deleteClientes(id)
+
+}
