79d284cd6c1a9252ee6ec33d6eb10b8ab71c901c
fahh7 <fahh7@FREIMYHP>
2025-06-12 00:15:58 -0400

    Data Source

---
+++ app/src/main/java/edu/ucne/registrotecnico/data/remote/retenciones/DataSource.kt
@@ -0,0 +1,19 @@
+rpackage edu.ucne.registrotecnico.data.remote.retenciones
+
+import edu.ucne.registrotecnico.data.remote.dto.RetencionDTO
+import javax.inject.Inject
+
+class DataSource @Inject constructor(
+    private val retencionesManagerApi: RetencionManajerApi
+){
+    suspend fun getRetenciones() = retencionesManagerApi.getRetenciones()
+
+    suspend fun getRetencion(id: Int) = retencionesManagerApi.getRetencion(id)
+
+    suspend fun saveRetencion(retencionDto: RetencionDTO) = retencionesManagerApi.saveRetencion(retencionDto)
+
+    suspend fun actualizarRetencion(id: Int, retencionDto: RetencionDTO) = retencionesManagerApi.actualizarRetencion(id, retencionDto)
+
+    suspend fun deleteRetencion(id: Int) = retencionesManagerApi.deleteRetencion(id)
+
+}
