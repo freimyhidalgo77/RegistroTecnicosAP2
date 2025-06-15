5ed04b139d78f8a3ef0b75484d2ba21475ca128c
fahh7 <fahh7@FREIMYHP>
2025-06-15 00:33:00 -0400

    RetencionListScreen.kt

---
+++ app/src/main/java/edu/ucne/registrotecnico/presentation/retencion/RetencionListScreen.kt
@@ -0,0 +1,278 @@
+package edu.ucne.registrotecnico.presentation.retencion
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Refresh
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import edu.ucne.registrotecnico.R
+import edu.ucne.registrotecnico.data.remote.dto.RetencionDTO
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
+
+@Composable
+fun ArticuloListScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    viewModel: RetencionViewModel = hiltViewModel(),
+    createRetencion: () -> Unit,
+    onEditRetencion: (Int) -> Unit,
+    onDeleteRetencion: (Int) -> Unit
+) {
+    val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val context = LocalContext.current
+    var lastretentionCount by remember { mutableStateOf(0) }
+
+    LaunchedEffect(Unit) {
+        viewModel.getRetencion()
+    }
+
+    LaunchedEffect(uiState.retenciones) {
+        if (uiState.retenciones.size > lastretentionCount) {
+            Toast.makeText(context, "Nueva retencion: ${uiState.retenciones.lastOrNull()?.descripcion}", Toast.LENGTH_LONG).show()
+        }
+        lastretentionCount = uiState.retenciones.size
+    }
+
+    RetencionListBodyScreen(
+        drawerState = drawerState,
+        scope = scope,
+        uiState = uiState,
+        createRetencion = createRetencion,
+        onEditRetencion = onEditRetencion,
+        onDeleteRetencion = onDeleteRetencion,
+        reloadRetenciones = { viewModel.getRetencion() }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RetencionListBodyScreen(
+    drawerState: DrawerState,
+    scope: CoroutineScope,
+    uiState: RetencionUIState,
+    createRetencion: () -> Unit,
+    onEditRetencion: (Int) ->  Unit,
+    onDeleteRetencion: (Int) -> Unit,
+    reloadRetenciones: () -> Unit
+) {
+    var find by remember { mutableStateOf("") }
+
+    val filtrarRetencion = uiState.retenciones.filter {
+        it.descripcion.contains(find, ignoreCase = true) ||
+                it.retencionId.toString().contains(find)
+    }
+
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(
+                        text = "Lista de Retenciones",
+                        style = TextStyle(
+                            fontSize = 22.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        )
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = { scope.launch { drawerState.open() } }) {
+
+                    }
+                },
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = Color(0xFF6200EE)
+                )
+            )
+        },
+
+        floatingActionButton = {
+            Box(
+                contentAlignment = Alignment.BottomEnd,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                FloatingActionButton(
+                    onClick = reloadRetenciones,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.BottomEnd)
+                        .offset(y = (-16).dp),
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = Color.White
+                ) {
+                    Icon(Icons.Filled.Refresh, contentDescription = "Recargar Artículos")
+                }
+
+                FloatingActionButton(
+                    onClick = createRetencion,
+                    modifier = Modifier
+                        .padding(16.dp)
+                        .align(Alignment.BottomEnd)
+                        .offset(y = (-80).dp),
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = Color.White
+                ) {
+                    Icon(Icons.Filled.Add, contentDescription = "Añadir Retencion")
+                }
+            }
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp)
+        ) {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                SearchFilter(find) { query -> find = query }
+
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(filtrarRetencion) { retencion ->
+                        RetencionRow(retencion, onEditRetencion, onDeleteRetencion)
+                    }
+                }
+            }
+
+            if (uiState.isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.align(Alignment.Center)
+                )
+            }
+        }
+    }
+}
+
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchFilter(
+    find: String,
+    onFindQueryChange: (String) -> Unit
+) {
+    TextField(
+        value = find,
+        onValueChange = { onFindQueryChange(it) },
+        label = { Text("Buscar retencion") },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 16.dp),
+        singleLine = true,
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = MaterialTheme.colorScheme.surface,
+            focusedIndicatorColor = MaterialTheme.colorScheme.primary,
+            unfocusedIndicatorColor = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.6f)
+        ),
+        leadingIcon = {
+            Icon(Icons.Filled.Search, contentDescription = "Buscar")
+        }
+    )
+}
+
+
+@Composable
+fun RetencionRow(
+    retencion: RetencionDTO,
+    onEditRetencion: (Int) -> Unit,
+    onDeleteRetencion: (Int) -> Unit
+) {
+    var expanded by remember { mutableStateOf(false) }
+
+    Card(
+        shape = MaterialTheme.shapes.medium,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        elevation = CardDefaults.cardElevation(6.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp)
+                .background(MaterialTheme.colorScheme.surfaceVariant),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column(
+                modifier = Modifier.weight(5f),
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text(
+                    text = "ArtículoId: ${retencion.retencionId}",
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.onSurface
+                    )
+                )
+                Text(
+                    text = "Descripción: ${retencion.descripcion}",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                    )
+                )
+                Text(
+                    text = "Costo: ${retencion.monto}",
+                    style = TextStyle(
+                        fontSize = 14.sp,
+                        color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.7f)
+                    )
+                )
+
+            }
+        }
+    }
+}
